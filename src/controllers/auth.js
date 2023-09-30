28a4238e157d6b5a201da26386e690cc5c08138b
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-09-30 13:15:51 -0600

    modifies login

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,20 @@
+const { users } = require("../database/models/index");
 
 
+async function login(email, password) {
+    const userData = await users.findOne({
+        where: { email }
+    });
 
-async function login(req, res) {
-    return {}
+    if (userData === null || !(userData.dataValues.password === password)) {
+        return { loggedIn: false, token: null };
+    }
+
+    const token = "PLACEHOLDER_TOKEN";
+
+    const { id, first_name, role, privileges } = userData;
+
+    return { loggedIn: true, id, token, first_name, role, privileges };
 }
 
 
