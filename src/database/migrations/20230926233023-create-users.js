381d242e30f2586d33e4be6dbfae6da51396267c
abrahamgmacias <abrahamgmacias@hotmail.com>
2023-09-30 13:16:04 -0600

    adds new model

---
+++ src/database/migrations/20230926233023-create-users.js
@@ -0,0 +1,72 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      second_name: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      second_last_name: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      birthday: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      phone_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      role_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      privileges: {
+        type: Sequelize.JSONB,
+        allowNull: false
+      },
+      active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: true,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
